1fd9a6c5f8e6443999faa2a6cb9eb4299e49fe61
smritapokharel <smrita.pokharel@gmail.com>
2025-05-12 21:13:59 +1000

    Starting to get bottom up a bit more :) :)

---
+++ DynamicProgramming/src/coinchange/CoinChange.java
@@ -0,0 +1,81 @@
+package coinchange;
+
+
+/**
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ *
+ * Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+ *
+ * You may assume that you have an infinite number of each kind of coin.
+ *
+ * https://leetcode.com/problems/coin-change-ii
+ */
+public class CoinChange {
+
+    int change(int amount, int[] coins) {
+
+        return topDownWithoutMemoization(amount, coins, 0);
+    }
+
+    int topDownWithoutMemoization(int amount, int[] coins, int index){
+
+        if(amount == 0) return 1;
+
+        if(amount < 0 || index >= coins.length) return 0;
+
+        int skip = topDownWithoutMemoization(amount, coins, index+1);
+
+        if(coins[index] > amount ) return skip;
+
+        int include = topDownWithoutMemoization(amount-coins[index], coins, index);
+
+        return include + skip;
+    }
+
+    int topDownWithMemoization(int amount, int[] coins, int index, int[][] memo){
+
+        if(amount == 0 ) return 1;
+
+        if(amount < 0 || index >= coins.length) return 0;
+
+        if(memo[index][amount]!=0) return memo[index][amount];
+
+        int skip = topDownWithMemoization(amount, coins, index+1, memo);
+
+        if(coins[index] > amount ) return skip;
+
+        int include = topDownWithMemoization(amount-coins[index], coins, index, memo);
+
+        memo[index][amount] = include + skip;
+
+        return memo[index][amount];
+    }
+
+    int bottomup(int amount, int[] coins){
+
+        int n = coins.length;
+
+        int[][] dp = new int[n+1][amount + 1];
+
+        for(int i =0; i< n; i++){
+
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i<= n ; i++){
+
+            int coin = coins[i-1];
+
+            for(int j = 1; j<= amount; j++){
+
+                if(coin> j){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - coin];
+                }
+            }
+        }
+
+        return dp[n][amount];
+    }
+}
