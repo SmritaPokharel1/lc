53fdfb27c9a1a4cd395811e15cfd146316c43891
smritapokharel <smrita.pokharel@gmail.com>
2025-06-01 20:57:11 +1000

    Bottom up for Triangle :)

--- LeetCode/src/matrix/Triangle.java
+++ LeetCode/src/matrix/Triangle.java
@@ -10,30 +10,65 @@ import java.util.List;
 // you may move to either index i or index i + 1 on the next row.
 public class Triangle {
 
-    public int minimumTotal(List<List<Integer>> triangle) {
+    public int minimumTotal(List<List<Integer>> triangle){
 
-        int size = triangle.size();
 
-        int[][] memo = new int[size][triangle.get(size-1).size()];
+        // int[][] memo = new int[size][triangle.get(size-1).size()];
 
-        for (int i = 0; i < size; i++) {
-            memo[i] = new int[triangle.get(i).size()];
-            Arrays.fill(memo[i], -1);
-        }
+        // for (int i = 0; i < size; i++) {
+        //     memo[i] = new int[triangle.get(i).size()];
+        //     Arrays.fill(memo[i], -1);
+        // }
 
-        return path(triangle, 0,0, memo);
+        return minTotalBottomUp(triangle);
     }
 
-    public int path(List<List<Integer>> triangle, int next, int i, int[][] memo){
+    public int minTotalBottomUp(List<List<Integer>> triangle){
 
-        if(next >= triangle.size() || i >= triangle.get(next).size()) return 0;
+        int m = triangle.size();
+        int n = triangle.get(m-1).size();
 
-        if(next==triangle.size() -1) return triangle.get(next).get(i);
+        int[][] dp = new int[m+1][n+1];
 
-        if(memo[next][i]!=-1) return memo[next][i];
+        for(int i =0; i<n;i++){
 
-        memo[next][i] = triangle.get(next).get(i) + Math.min(path(triangle,next+1,i,memo), path(triangle,next+1, i+1,memo));
+            dp[m-1][i] = triangle.get(m-1).get(i);
+        }
 
-        return memo[next][i];
+        for(int i = m-2; i>=0; i--){
+
+            for(int j = 0; j < triangle.get(i).size(); j++){
+
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public int minTotalWithMemo(List<List<Integer>> triangle, int i, int j, int[][] memo){
+
+        if( i >= triangle.size()) return 0;
+
+        if( i== triangle.size() -1 && j >= triangle.get(i).size()) return 0;
+
+        if(i== triangle.size() -1) return triangle.get(i).get(j);
+
+        if(memo[i][j]!=-1) return memo[i][j];
+
+        memo[i][j] = triangle.get(i).get(j) + Math.min(minTotalWithMemo(triangle, i+1, j, memo), minTotalWithMemo(triangle, i+1, j+1, memo));
+
+        return memo[i][j];
+    }
+
+    public int minTotalWithoutMemo(List<List<Integer>> triangle, int i, int j){
+
+        if(i >= triangle.size()) return 0;
+
+        if(i == triangle.size() -1 && j > triangle.get(i).size()) return 0;
+
+        if( i == triangle.size() -1) return triangle.get(i).get(j);
+
+        return triangle.get(i).get(j) + Math.min(minTotalWithoutMemo(triangle, i+1, j), minTotalWithoutMemo(triangle, i+1, j+1));
     }
 }
