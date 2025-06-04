50ad07546cb84e99c5ead27356e9d4bea5c31682
smritapokharel <smrita.pokharel@gmail.com>
2025-06-04 12:39:36 +1000

    Formulae 🫠

---
+++ LeetCode/src/trees/UniqueBST.java
@@ -0,0 +1,47 @@
+package trees;
+
+//https://leetcode.com/problems/unique-binary-search-trees
+//https://medium.com/@smrita/catalan-number-42b33ae2c2ca
+public class UniqueBST {
+
+    public int numTrees(int n) {
+
+        return catalan(n);
+    }
+
+    public int catalan(int n){
+
+        long C = 1;
+
+        for(int i =0; i < n; ++i){
+
+           C = (C * 2 * (2 * i + 1)) / (i + 2);
+        }
+
+        return (int) C;
+    }
+
+    public int catalanRecursive(int n){
+
+        if(n <= 1) return 1;
+
+        int result = 0;
+
+        for(int i =0; i < n; i++){
+
+            result += catalan(i) * catalan(n - i - 1);
+        }
+
+        return result;
+    }
+
+    public long fact(int n){
+        long fact = 1;
+        for(int i=1;i<=n;i++){
+
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
