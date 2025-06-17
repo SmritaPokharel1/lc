19e93f98a42569a840dae4b0837d6ef21ad87926
smritapokharel <smrita.pokharel@gmail.com>
2025-06-17 16:45:32 +1000

    Is list a palindrome

--- LeetCode/src/linkedlist/IsPalindrome.java
+++ LeetCode/src/linkedlist/IsPalindrome.java
@@ -6,34 +6,29 @@ public class IsPalindrome {
 
     public boolean isPanlindrome(ListNode head){
 
-        if(head==null || head.next==null) return true;
+        if(head==null || head.next== null) return true;
 
         ListNode slow = head;
         ListNode fast = head;
-
         Stack<Integer> stack = new Stack<Integer>();
 
-        while(fast!=null && fast.next!=null){
+        while(fast !=null && fast.next != null){
+
+            stack.push(slow.val);
 
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        if (fast != null) {
+        if(fast != null){
+
             slow = slow.next;
         }
 
         while(slow!=null){
 
-            stack.push(slow.val);
-            slow = slow.next;
-        }
-
-        while(!stack.isEmpty()){
-
-            if(head.val!=stack.pop()) return false;
-
-            head = head.next;
+            if(slow.val != stack.pop()) return false;
+            slow= slow.next;
         }
 
         return true;
