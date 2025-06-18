8984df65b422a4f398d5aa3df96d46cdf82514b2
smritapokharel <smrita.pokharel@gmail.com>
2025-06-19 06:53:36 +1000

    Refactor

--- LeetCode/src/Queues/WallsAndGates.java
+++ LeetCode/src/Queues/WallsAndGates.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 
 public class WallsAndGates {
 
-    boolean[][] visited;
+    private boolean[][] visited;
 
     private static final int GATE = 0;
 
@@ -15,93 +15,89 @@ public class WallsAndGates {
 
     private static final List<int[]> DIRECTIONS = Arrays.asList(
 
-            new int[] {1,0},
-            new int[] {-1,0},
-            new int[] {0,1},
-            new int[] {0,-1}
+            new int[]{1, 0},
+            new int[]{-1, 0},
+            new int[]{0, 1},
+            new int[]{0, -1}
     );
 
     public void wallsAndGates(int[][] rooms) {
-        bfs(rooms);
+        wallsAndGatesBfs(rooms);
     }
 
-    public void bfs(int[][] rooms){
+    public void wallsAndGatesBfs(int[][] rooms) {
 
         int m = rooms.length;
         int n = rooms[0].length;
 
         Queue<int[]> queue = new LinkedList<>();
 
-        for(int i =0; i < m; i++){
+        for (int i = 0; i < m; i++) {
 
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
 
-                if(rooms[i][j] == GATE){
+                if (rooms[i][j] == GATE) {
 
-                    queue.add(new int[]{i,j});
+                    queue.add(new int[]{i, j});
                 }
             }
         }
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
 
             int[] point = queue.remove();
 
             int row = point[0];
             int col = point[1];
 
-            for(int[] direction : DIRECTIONS){
+            for (int[] direction : DIRECTIONS) {
 
                 int r = row + direction[0];
                 int c = col + direction[1];
 
-                if( r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) continue;
+                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) continue;
 
-                //rooms[r][c] = Math.min(rooms[r][c],rooms[row][col] + 1);
                 rooms[r][c] = rooms[row][col] + 1;
 
-                queue.add(new int[] {r,c});
+                queue.add(new int[]{r, c});
             }
         }
     }
-
-
-
 
 
     public void wallsAndGatesDfs(int[][] rooms) {
 
-        int m  = rooms.length;
+        int m = rooms.length;
         int n = rooms[0].length;
 
         visited = new boolean[m][n];
 
-        for(int i =0; i < m; i++){
+        for (int i = 0; i < m; i++) {
 
-            for(int j =0; j < n; j++){
+            for (int j = 0; j < n; j++) {
 
-                if(rooms[i][j]==0) dfs(rooms,i,j,0);
+                if (rooms[i][j] == 0) dfs(rooms, i, j, 0);
             }
         }
 
     }
 
-    public void dfs(int[][] rooms, int i, int j , int distance){
+    public void dfs(int[][] rooms, int i, int j, int distance) {
 
         int m = rooms.length;
         int n = rooms[0].length;
 
-        if(i >= m || j >= n || i < 0 || j < 0) return;
+        if (i >= m || j >= n || i < 0 || j < 0) return;
 
-        if(rooms[i][j] < distance) return;
+        if (rooms[i][j] < distance) return;
 
-        if(rooms[i][j] == -1) return;
+        if (rooms[i][j] == -1) return;
 
         rooms[i][j] = distance;
 
-        dfs(rooms,i-1,j, distance + 1);
-        dfs(rooms,i, j-1, distance + 1);
-        dfs(rooms, i+1, j, distance + 1);
-        dfs(rooms, i, j+1, distance + 1);
+        dfs(rooms, i - 1, j, distance + 1);
+        dfs(rooms, i, j - 1, distance + 1);
+        dfs(rooms, i + 1, j, distance + 1);
+        dfs(rooms, i, j + 1, distance + 1);
     }
 }
