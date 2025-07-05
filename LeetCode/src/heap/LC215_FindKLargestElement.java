0a6511942c8790ac6dd9266205bb1a3733c30c24
smritapokharel <smrita.pokharel@gmail.com>
2025-07-05 13:53:09 +1000

    Description

--- LeetCode/src/heap/LC215_FindKLargestElement.java
+++ LeetCode/src/heap/LC215_FindKLargestElement.java
@@ -2,6 +2,26 @@ package heap;
 
 import java.util.PriorityQueue;
 
+/**
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+ *
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Can you solve it without sorting?
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [3,2,1,5,6,4], k = 2
+ * Output: 5
+ * Example 2:
+ *
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+ * Output: 4
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ */
 public class LC215_FindKLargestElement {
 
     public int findKthLargest(int[] nums, int k) {
