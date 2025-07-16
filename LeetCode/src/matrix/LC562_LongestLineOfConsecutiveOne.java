745d6ffa2965b38793a8c2c8d5109cd4e41b7b46
smritapokharel <smrita.pokharel@gmail.com>
2025-07-16 11:28:04 +1000

    Longest Line of consecutive ones

---
+++ LeetCode/src/matrix/LC562_LongestLineOfConsecutiveOne.java
@@ -0,0 +1,44 @@
+package matrix;
+
+public class LC562_LongestLineOfConsecutiveOne {
+
+    public int longestLine(int[][] mat) {
+
+        int max = Integer.MIN_VALUE;
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for(int i = 0; i < m; i++){
+
+            for(int j = 0; j < n; j++){
+
+                if(mat[i][j]==1){
+
+                    max = Math.max(max,dfs(mat,i,j,0, 1));
+                    max = Math.max(max,dfs(mat,i,j, 1, 0));
+                    max = Math.max(max,dfs(mat,i, j, 1,1));
+                    max = Math.max(max,dfs(mat, i, j, 1, -1));
+
+                }
+            }
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+
+    public int dfs(int[][] mat, int i , int j, int dx, int dy){
+
+        int m = mat.length;
+        int n = mat[0].length;
+        int count = 0;
+
+        while(i >=0 && j >=0 && i < m && j < n && mat[i][j] == 1){
+
+            count ++;
+            i += dx;
+            j += dy;
+        }
+
+        return count;
+    }
+}
