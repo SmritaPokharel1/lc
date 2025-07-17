5242e11093ecdea5bcad3687f5e5b74c645464eb
smritapokharel <smrita.pokharel@gmail.com>
2025-07-17 15:50:50 +1000

    Bottom up for edit distance

--- LeetCode/src/dynamicprogramming/strings/LC72_EditDistance.java
+++ LeetCode/src/dynamicprogramming/strings/LC72_EditDistance.java
@@ -42,6 +42,40 @@ public class LC72_EditDistance {
         return minDistanceR(word1, word2, word1.length(), word2.length(), memo);
     }
 
+    public int bottomup(String word1, String word2){
+
+        int col = word1.length();
+
+        int row = word2.length();
+
+        int[][] dp = new int[row+1][col+1];
+
+        for(int i = 0 ; i <= row; i++){
+
+            dp[i][0] = i;
+        }
+
+        for(int j = 0; j <= col; j++){
+
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i <= row; i++){
+
+            for(int j = 1; j <= col; j++){
+
+                if(word2.charAt(i-1)==word1.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+
+                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) + 1;
+                }
+            }
+        }
+
+        return dp[row][col];
+    }
+
     public int minDistanceR(String word1, String word2, int l1, int l2, Integer[][] memo){
 
         if(l1==0) return l2;
