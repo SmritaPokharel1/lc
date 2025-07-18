b8d261ce2bb11845aa14bc3d3d81f621c838200c
smritapokharel <smrita.pokharel@gmail.com>
2025-07-19 09:18:53 +1000

    Bottom up for maximal square

--- LeetCode/src/dynamicprogramming/matrix/LC221_MaximalSquare.java
+++ LeetCode/src/dynamicprogramming/matrix/LC221_MaximalSquare.java
@@ -2,33 +2,52 @@ package dynamicprogramming.matrix;
 
 import java.util.Arrays;
 
+/**
+ * Given an m x n binary matrix filled with 0's and 1's,
+ * find the largest square containing only 1's and return its area.
+ *
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 4
+ *
+ * Input: matrix = [["0","1"],["1","0"]]
+ * Output: 1
+ *
+ *
+ * Input: matrix = [["0"]]
+ * Output: 0
+ *
+ * https://leetcode.com/problems/maximal-square/description/
+ *
+ */
 public class LC221_MaximalSquare {
 
-    private int maxArea = Integer.MIN_VALUE;
-
-    public int maximalSquare(char[][] matrix) {
+    public int bottomup(char[][] matrix){
 
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] memo = new int[m][n];
 
-        for(int i =0; i < m; i++){
+        int maxSide = 0;
 
-            Arrays.fill(memo[i], -1);
-        }
+        int[][] dp = new int[m][n];
 
-        for(int i =0; i < m; i ++){
+        for(int i = m-1; i >= 0; i--){
 
-            for(int j = 0; j < n; j++){
+            for(int j = n-1; j >=0; j--){
 
-                if(matrix[i][j]=='1'){
+                if(matrix[i][j] == '1'){
 
-                    maxArea = Math.max(maxArea,solve(matrix,i,j,memo));
+                    int right = (j+1 < n) ? dp[i][j+1] : 0;
+                    int down = (i+1 < m) ? dp[i+1][j] : 0;
+                    int diag = (i+1 < m && j+1 < n) ? dp[i+1][j+1] : 0;
+
+                    dp[i][j] = 1 + Math.min(right, Math.min(down,diag));
+
+                    maxSide = Math.max(maxSide, dp[i][j]);
                 }
             }
         }
 
-        return maxArea * maxArea;
+        return maxSide * maxSide;
     }
 
     public int solve(char[][] matrix, int row, int col, int[][] memo){
