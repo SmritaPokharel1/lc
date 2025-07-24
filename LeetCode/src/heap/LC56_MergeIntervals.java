0c379fff20abb5bef2583b60b66db9348804ea33
smritapokharel <smrita.pokharel@gmail.com>
2025-07-24 15:32:25 +1000

    Merge intervals

--- LeetCode/src/heap/LC56_MergeIntervals.java
+++ LeetCode/src/heap/LC56_MergeIntervals.java
@@ -27,7 +27,7 @@ import java.util.PriorityQueue;
  */
 public class LC56_MergeIntervals {
 
-    public int[][] merge(int[][] intervals) {
+    public int[][] mergeI(int[][] intervals) {
 
         if (intervals.length == 0) return new int[0][];
 
@@ -97,6 +97,33 @@ public class LC56_MergeIntervals {
 
             result.add(new int[]{start, end});
         }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        int[] current = intervals[0];
+
+        List<int[]> result = new ArrayList<>();
+
+        for(int i = 1; i < intervals.length; i++){
+
+            int[] next = intervals[i];
+
+            if(current[1] >= next[0]){
+
+                current[1] = Math.max(current[1],next[1]);
+            }else{
+
+                result.add(current);
+                current = next;
+            }
+        }
+
+        result.add(current);
 
         return result.toArray(new int[result.size()][]);
     }
